a55b4b77c08e8052cf206f37e04e5714d2921e67
hariprasath94 <49122331+hariprasath94@users.noreply.github.com>
2020-02-19 10:19:31 +0530

    New file

--- CountingPairs.java
+++ CountingPairs.java
@@ -1,32 +1,40 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
- 
-class CountPairs{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long inputArr[] = new long[n];
-		for(int i = 0; i < n;i++) {
-			inputArr[i] = sc.nextLong();
-		}
-		long k = sc.nextLong();
-		System.out.println(countPairs(inputArr,k));
-	}
- 
-	private static int countPairs(long[] inputArr, long k) {
-		int count = 0;
-		Set<Long> dataSet = new HashSet<>();
-		for(int i = 0; i < inputArr.length;i++) {
-			dataSet.add(inputArr[i]);
-		}
- 
-		for(Long d : dataSet) {
-			if (dataSet.contains(d + k)) {
-				++count;
-			}
-		}
- 
-		return count;
-	}
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MudWall {
+
+public static int maxHeight(List<Integer> stickPositions, List<Integer> stickHeights) {
+    int n = stickPositions.size();
+    int max = 0;
+
+    for (int i=0; i<n-1; i++) {
+        if (stickPositions.get(i)<stickPositions.get(i+1)-1) {
+            int hytDiff =  stickHeights.get(i+1) - stickHeights.get(i);
+            int noOfMuds = stickPositions.get(i+1) - stickPositions.get(i) - 1;
+            int tempValue = 0;
+            if (noOfMuds > hytDiff) {
+                int low = Math.max(stickHeights.get(i+1), stickHeights.get(i)) + 1;
+                int remainingGap = noOfMuds - hytDiff - 1;
+                tempValue = low + remainingGap/2;
+
+            } else {
+                tempValue = Math.min(stickHeights.get(i+1), stickHeights.get(i)) + noOfMuds;
+            }
+
+            max = Math.max(max, tempValue);
+        }
+    }
+
+    return max;
+}
+
+public static void main(String[] args){
+    List<Integer> sPosition =  Arrays.asList(7,12,16,22,26,29);
+    List<Integer> sHeights =  Arrays.asList(8,8,9,8,8,11);
+    System.out.println(maxHeight(sPosition, sHeights));
+}
+
+
 }
